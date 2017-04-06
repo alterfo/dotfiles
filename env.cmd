9c5e0d940d171a5c985bdbc10b2bec73ddefd00e
Oleg <kalinon7@gmail.com>
2017-04-06 09:57:29 +0300

    gitconfig

--- env.cmd
+++ env.cmd
@@ -1,6 +1,6 @@
-DOSKEY gbr=grunt build release
+DOSKEY gbr=svn update ^& grunt build release
 DOSKEY gs=grunt serve
 DOSKEY cds=cd C:\Sites
 DOSKEY o=explorer .
-DOSKEY gp=git push
 DOSKEY ub=rm -rf vendor ^& bower i
+DOSKEY upr=npm i ^& bower i ^& svn update ^& grunt build release
